366f98f4b5e35e0e2c82e18a2d846dd7712ecfcb
Sai Kiran <skiran.mekala@gmail.com>
2023-12-02 17:32:20 +0000

    all products fetch in homepage controller

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,16 +1,33 @@
-const {db_connection} = require('../utils/database')
+const { db_connection } = require("../utils/database");
 
-exports.getHomeDetails = async (req, res) => {
-  let products = {}; // ProductName,Price,Description,EntryDate,Age,Category,Size
+exports.getHomeDetails = (req, res) => {
+  let products_obj = []; // ProductName,Price,Description,EntryDate,Age,Category,Size
   let sale_products = {}; // ProductName,Price,Description,EntryDate,Age,Category,Size
 
-  db_conn = await db_connection
-  db_conn.query(
-    "select * from Product",
-    (err, result) => {
-      console.log(result);
-      res.send(JSON.stringify(result));
-    }
-    );
+  db_connection.then((db_conn) => {
+    db_conn.query("select * from Product").then(async (products) => {
+      let product_entries = products["recordset"];
+      for (let ele of product_entries) {
 
+        img_obj = await db_conn.query(
+          `select ImgLink from Image where ImgId = '${ele["ImgId"]}'`
+        );
+        let img_entry = img_obj["recordset"];
+        ele["ImgLink"] = img_entry[0]["ImgLink"];
+
+        insp_obj = await db_conn.query(
+          `select InspectionDate,Rating,Comments,ApparelExpertID from Inspection where InspectionID = '${ele["InspectionID"]}'`
+        );
+        let insp_entry = insp_obj["recordset"][0];
+        ele["InspectionDate"] = insp_entry["InspectionDate"];
+        ele["Rating"] = insp_entry["Rating"];
+        ele["Comments"] = insp_entry["Comments"];
+        ele["ApparelExpertID"] = insp_entry["ApparelExpertID"];
+
+      }
+      products_obj = product_entries;
+      console.log(products_obj);
+      res.send(JSON.stringify(products_obj));
+    });
+  });
 };
--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,13 @@
 const mssql = require("mssql");
 require("dotenv").config();
 
-
-const db_connection = mssql.connect(
+let db_connection = mssql.connect(
+// const db_connection = mssql.connect(
   `Driver={ODBC Driver 18 for SQL Server};Server=${process.env.DB_SERVER};Database=${process.env.DB_NAME};Uid=${process.env.DB_USER};Pwd=${process.env.DB_PWD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;`
 ).then(pool=>{
+    // db_connection=pool
     console.log(pool)
+    console.log("successfully connected to MS SQL!")
     return pool
 }).catch(err=>{
     console.log(err)
