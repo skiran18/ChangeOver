9dd3f38bf5233f638ee88d69ab33b9af5fb1c6ad
HennaKausar <hennakausarformal@gmail.com>
2023-12-07 16:08:05 +0000

    handling errors in category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,16 +1,35 @@
 const { db_connection } = require("../utils/database");
+const { fetchImageInsp } = require("./homepageController");
 
 exports.getMenProducts = async (req, res) => {
+    let cat_products_obj = {};
+    db_connection.then(async (db_conn) => {
     let err_all_products;
-    await db_conn
-      .query(`select * from Product where Category = '${req.params.category}'`)
-      .then(async (products) => {
-        let product_entries = products["recordset"];
-        cat_products_obj["categoryProducts"] = await fetchImageInsp(
-          db_conn,
-          product_entries
-        );
-        res.set("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify(cat_products_obj));
-      })
+        await db_conn
+            .query(`select * from Product where Category = '${req.params.category}'`)
+            .then(async (products) => {
+                let product_entries = products["recordset"];
+                cat_products_obj["categoryProducts"] = await fetchImageInsp(
+                    db_conn,
+                    product_entries
+                );
+                res.set("Content-Type", "application/json");
+                res.status(200).send(JSON.stringify(cat_products_obj));
+            })
+            .catch((err) => {
+                err_all_products = true;
+                console.log(err);
+                res.set("Content-Type", "application/json");
+                res.status(500).send(
+                    JSON.stringify({
+                        errorStatus: 500,
+                        error: "Error while quering for all products",
+                    })
+                );
+                return;
+            });
+        if (err_all_products) {
+            return;
+        }
+    });
 };
