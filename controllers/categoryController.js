556082dcdb8f1da228a2a3736779981ce1612edc
HennaKausar <hennakausarformal@gmail.com>
2023-12-07 16:03:37 +0000

    query to select product from category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,16 @@
 const { db_connection } = require("../utils/database");
 
 exports.getMenProducts = async (req, res) => {
+    let err_all_products;
+    await db_conn
+      .query(`select * from Product where Category = '${req.params.category}'`)
+      .then(async (products) => {
+        let product_entries = products["recordset"];
+        cat_products_obj["categoryProducts"] = await fetchImageInsp(
+          db_conn,
+          product_entries
+        );
+        res.set("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify(cat_products_obj));
+      })
 };
