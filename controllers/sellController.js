c1dcbe6ae8d2287caa30c019e16323fecd61839c
Sai Kiran <skiran.mekala@gmail.com>
2023-12-11 13:12:12 +0000

    InspectionOrder query completed in sellcontroller

--- controllers/sellController.js
+++ controllers/sellController.js
@@ -4,19 +4,29 @@ const createProduct = async (req, res) => {
 
   let customer_details = {}
   console.log(req.body)
-  // customer_details.CustomerName
-  // customer_details.PhoneNumber
-  // customer_details.EmailID
-  // customer_details.Address
 
   db_connection.then(async (db_conn) => {
-    // let err_all_products
+    let inspectionId = ''
     await db_conn
       .query(`declare @count as int
       set @count= (Select COUNT(CustomerID) FROM Customer)+1
       Insert INTO Customer
       VALUES (@count,'${req.body.CustomerName}','${req.body.PhoneNumber}','${req.body.EmailID}','${req.body.Address}')`)
       .then(async (result) => {
+        if(result.rowsAffected){
+          await db_conn.query(`declare @count_inspord as int
+          set @count_inspord= (Select COUNT(InspectionOrderID) FROM InspectionOrder)+1
+          Insert INTO KiranDB.dbo.InspectionOrder
+          VALUES (@count_inspord,'10','not done')`)
+          .then((result_insp)=>{
+            db_conn.query(
+            `select TOP 1 * from InspectionOrder ORDER BY InspectionOrderID Desc`)
+            .then((res) => {
+              inspectionId = res["recordset"][0]['InspectionOrderID']
+              console.log(res)
+            })
+          })
+        }
         console.log(result)
         res.send(result)
       })
