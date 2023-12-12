91bf16110167d23a4215241008b5874b9fdc1b3d
ShirsatShruti <shrutishirsat95@gmail.com>
2023-12-12 22:46:21 +0000

    Adding CORS middleware to fix bug in FE

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const homepageRouter = require("./routes/homePage");
 const categoryRouter = require("./routes/categoryRoutes");
 const sellRouter = require("./routes/sellRoutes");
 
+var corsOptions = function(req, res, next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+}
+
+app.use(corsOptions);
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
