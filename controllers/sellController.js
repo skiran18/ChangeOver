5013e73f4261b0d16760453e25cb6bc1b1a26d5b
HennaKausar <hennakausarformal@gmail.com>
2023-12-12 12:46:15 +0000

    sellOrder table query

--- controllers/sellController.js
+++ controllers/sellController.js
@@ -18,35 +18,46 @@ const createProduct = async (req, res) => {
           set @count_inspord= (Select COUNT(InspectionOrderID) FROM InspectionOrder)+1
           Insert INTO KiranDB.dbo.InspectionOrder
           VALUES (@count_inspord,'10','not done')`)
-          .then((result_insp)=>{
+          .then(()=>{
             db_conn.query(
             `select TOP 1 * from InspectionOrder ORDER BY InspectionOrderID Desc`)
-            .then((res) => {
-              inspectionId = res["recordset"][0]['InspectionOrderID']
-              console.log(res)
+            .then((result_insp) => {
+              inspectionId = result_insp["recordset"][0]['InspectionOrderID']
+              console.log(result_insp)
+              db_conn.query(`declare @count_sellorder as int
+              set @count_sellorder= (Select COUNT(OrderId) FROM SellOrder)+1
+              Insert INTO KiranDB.dbo.SellOrder
+              VALUES (@count_sellorder,'${req.body.ProductName}','${req.body.Size}','${req.body.Category}','${req.body.PriceNegotiable}','${req.body.Age}','${req.body.PriceQuoted}','${inspectionId}')`)
+              .then((final_res)=>{
+                console.log(final_res)
+                if (final_res.rowsAffected){
+                  res.set("Content-Type", "application/json");
+                  res.setHeader('Access-Control-Allow-Origin', '*');
+                  res.status(200).send(JSON.stringify({
+                    status: 200,
+                    message: "Successfully submitted the sell product request",
+                  }));
+                }
+              })
             })
           })
         }
-        console.log(result)
-        res.send(result)
       })
       .catch((err) => {
         err_all_products = true
         console.log(err);
         res.set("Content-Type", "application/json");
+        res.setHeader('Access-Control-Allow-Origin', '*');
         res
           .status(500)
           .send(
             JSON.stringify({
               errorStatus: 500,
-              error: "Error while quering for all products",
+              error: "Error while submitting sell product details",
             })
           );
         return;
       });
-      // if (err_all_products){
-      //   return
-      // }
   });
 };
 
